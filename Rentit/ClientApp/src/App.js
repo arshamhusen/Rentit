bf3c198fdfc6b0fab4843b14ce6903c66e45daeb
JANICE LIE <TP051250@mail.apu.edu.my>
2021-07-06 21:37:52 +0800

    second test

--- Rentit/ClientApp/src/App.js
+++ Rentit/ClientApp/src/App.js
@@ -9,7 +9,7 @@ import './custom.css'
 
 export default class App extends Component {
   static displayName = App.name;
-    //hiii
+    //hiii second test
   render () {
     return (
       <Layout>
